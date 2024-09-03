8e9b324cef9fd5a8b41ca94f2f0c6cb9f8a4ff57
Adam Sherwood <adam.the.sherwood@gmail.com>
2024-09-03 11:41:57 +0100

    render multiple editors

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,13 @@ function is_dom_in_viewport(el) {
   return 0;
 }
 
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+// create a schema with list support.
+const my_schema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks,
+});
+
 function binary_search(
   arr,
   // 0 is match, negative means search right of element, positive means search left of element
@@ -68,12 +75,11 @@ function binary_search(
   return -1;
 }
 
-function get_virtual_list_window(doc: Node, mapping, buffer = 20) {
+function get_virtual_list_window(view, doc: Node, mapping, buffer = 20) {
   let frag = doc.content;
   let data: { node: Node; offset: number; idx: number }[] = [];
   frag.forEach((node, offset, idx) => {
-    data.push({ node, offset: false ? mapping.map(offset) : offset, idx });
-    // data.push({ node, offset: mapping ? mapping.map(offset) : offset, idx });
+    data.push({ node, offset, idx });
   });
   console.log("FRAG", data, mapping);
   function pred(datum: { node: Node; offset: number; idx: number }, i) {
@@ -102,87 +108,99 @@ function get_virtual_list_window(doc: Node, mapping, buffer = 20) {
   return res;
 }
 
+const TOP_LEVEL_DEC_META = "top_level_dec";
+
 function update_top_level_node_decs(view) {
   let { state, dispatch } = view;
   let tx = state.tr;
-  tx.setMeta("top_level_dec", true);
+  tx.setMeta(TOP_LEVEL_DEC_META, true);
   tx.setMeta("addToHistory", false);
   dispatch(tx);
 }
 
-let top_level_node_plugin = new Plugin({
-  state: {
-    init(_, { doc }) {
-      console.log(doc);
-      let top_level_node_decs: Decoration[] = [];
-      let set = DecorationSet.create(doc, top_level_node_decs);
-      setTimeout(() => {
-        update_top_level_node_decs(view);
-      }, 0);
-      return set;
-    },
-    apply(tr, set) {
-      if (!tr.getMeta("top_level_dec")) {
+function setup_editor(root) {
+  let top_level_node_plugin = new Plugin({
+    state: {
+      init(_, { doc }) {
+        console.log(doc);
+        let top_level_node_decs: Decoration[] = [];
+        let set = DecorationSet.create(doc, top_level_node_decs);
         setTimeout(() => {
           update_top_level_node_decs(view);
         }, 0);
         return set;
-      }
-      console.group("APPLY", tr.doc, set, tr);
-      let { start, end, data } = get_virtual_list_window(
-        tr.doc,
-        tr.mapping.invert()
-      );
-      let top_level_node_decs: Decoration[] = [];
-      // let new_set = set.map(tr.mapping, tr.doc);
-      let new_set = DecorationSet.empty;
-      for (let i = start; i < end; i++) {
-        let count = i;
-        let { node, offset } = data[i];
-        let dec = Decoration.node(offset, offset + node.nodeSize, {
-          style: "background: yellow",
-          class: "top_level_dec",
-          "data-top_level_dec_count": count + "",
-        });
-        console.log(i, offset, node, node.nodeSize);
-        top_level_node_decs.push(dec);
-      }
-      console.groupEnd();
-      return new_set.add(tr.doc, top_level_node_decs);
+      },
+      apply(tr, set) {
+        if (!tr.getMeta(TOP_LEVEL_DEC_META)) {
+          setTimeout(() => {
+            update_top_level_node_decs(view);
+          }, 0);
+          return set;
+        }
+        console.group("APPLY", tr.doc, set, tr);
+        let { start, end, data } = get_virtual_list_window(
+          view,
+          tr.doc,
+          tr.mapping.invert()
+        );
+        let top_level_node_decs: Decoration[] = [];
+        // let new_set = set.map(tr.mapping, tr.doc);
+        let new_set = DecorationSet.empty;
+        for (let i = start; i < end; i++) {
+          let count = i;
+          let { node, offset } = data[i];
+          let dec = Decoration.node(offset, offset + node.nodeSize, {
+            style: "background: yellow",
+            class: "top_level_dec",
+            "data-top_level_dec_count": count + "",
+          });
+          console.log(i, offset, node, node.nodeSize);
+          top_level_node_decs.push(dec);
+        }
+        console.groupEnd();
+        return new_set.add(tr.doc, top_level_node_decs);
+      },
     },
-  },
-  props: {
-    decorations(state) {
-      return top_level_node_plugin.getState(state);
+    props: {
+      decorations(state) {
+        return top_level_node_plugin.getState(state);
+      },
     },
-  },
-});
+  });
 
-window.addEventListener("scroll", (e) => {
-  update_top_level_node_decs(view);
-});
+  let root_element = document.querySelector(root);
+  let editor_container = document.createElement("div");
+  root_element.appendChild(editor_container);
+  let editor = document.createElement("div");
+  editor.className = "editor";
+  let controls = document.createElement("div");
+  controls.className = "controls";
+  editor_container.appendChild(controls);
+  editor_container.appendChild(editor);
 
-// Mix the nodes from prosemirror-schema-list into the basic schema to
-// create a schema with list support.
-const my_schema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks,
-});
-
-let view = new EditorView(document.querySelector("#editor"), {
-  state: EditorState.create({
-    doc: DOMParser.fromSchema(my_schema).parse(
-      document.querySelector("#content")
-    ),
-    plugins: exampleSetup({
-      schema: my_schema,
-      plugins: [top_level_node_plugin],
+  let view = new EditorView(editor, {
+    state: EditorState.create({
+      doc: DOMParser.fromSchema(my_schema).parse(
+        document.querySelector("#content")
+      ),
+      plugins: exampleSetup({
+        schema: my_schema,
+        plugins: [top_level_node_plugin],
+      }),
     }),
-  }),
-});
-window.view = view;
+  });
 
-function insert_node(node, from, to?) {
+  create_lorem_button(view, controls, 1);
+  create_lorem_button(view, controls, 5);
+  create_lorem_button(view, controls, 10);
+  create_lorem_button(view, controls, 50);
+  create_lorem_button(view, controls, 1000);
+  create_lorem_button(view, controls, 10000);
+
+  return view;
+}
+
+function insert_node(view, node, from, to?) {
   if (to === undefined) to = from;
   const { state, dispatch } = view;
   // Create a transaction to replace the current selection with the new node
@@ -191,20 +209,20 @@ function insert_node(node, from, to?) {
   dispatch(transaction);
 }
 
-function insert_node_at_selection(node) {
+function insert_node_at_selection(view, node) {
   const { state } = view;
   const { selection } = state;
   const { from, to } = selection;
-  insert_node(node, from, to);
+  insert_node(view, node, from, to);
 }
 
-function insert_node_at_end(node) {
+function insert_node_at_end(view, node) {
   const { state } = view;
   let end = state.doc.content.size;
-  insert_node(node, end, end);
+  insert_node(view, node, end, end);
 }
 
-function insert_text(text, from, to?) {
+function insert_text(view, text, from, to?) {
   if (to === undefined) to = from;
   const { state, dispatch } = view;
   // Create a transaction to insert text at the current selection
@@ -213,17 +231,17 @@ function insert_text(text, from, to?) {
   dispatch(transaction);
 }
 
-function insert_text_at_selection(text) {
+function insert_text_at_selection(view, text) {
   const { state } = view;
   const { selection } = state;
   const { from, to } = selection;
-  insert_text(text, from, to);
+  insert_text(view, text, from, to);
 }
 
-function insert_text_at_end(text) {
+function insert_text_at_end(view, text) {
   const { state } = view;
   let end = state.doc.content.size;
-  insert_text(text, end, end);
+  insert_text(view, text, end, end);
 }
 
 function text_to_paragraph_nodes(text: string) {
@@ -234,25 +252,24 @@ function text_to_paragraph_nodes(text: string) {
   return paras;
 }
 
-function insert_lorem_ipsum(n: number) {
+function insert_lorem_ipsum(view, n: number) {
   let paras = lorem(n).flatMap((text) => text_to_paragraph_nodes(text));
-  if (view.hasFocus()) insert_node_at_selection(Fragment.from(paras));
-  else insert_node_at_end(Fragment.from(paras));
+  if (view.hasFocus()) insert_node_at_selection(view, Fragment.from(paras));
+  else insert_node_at_end(view, Fragment.from(paras));
 }
 
-function create_lorem_button(n: number) {
-  let controls_div = document.querySelector("#controls");
+function create_lorem_button(view, controls_container, n: number) {
   let lorem_button = document.createElement("button");
   lorem_button.innerHTML = "Lorem " + n;
   lorem_button.addEventListener("pointerdown", (e) => {
     e.preventDefault();
-    insert_lorem_ipsum(n);
+    insert_lorem_ipsum(view, n);
   });
-  controls_div?.appendChild(lorem_button);
+  controls_container?.appendChild(lorem_button);
 }
 
-create_lorem_button(1);
-create_lorem_button(5);
-create_lorem_button(10);
-create_lorem_button(50);
-create_lorem_button(1000);
+let views = [setup_editor("#root"), setup_editor("#root")];
+
+window.addEventListener("scroll", (e) => {
+  views.forEach(update_top_level_node_decs);
+});
